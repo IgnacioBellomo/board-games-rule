1f44e31eda4e76e5a75ea0081c5da2c07fb15a75
--replace-all <ignaciobellomo@gmail.com>
2019-11-20 14:41:44 -0500

    Updates

---
+++ precache-manifest.0e5224449cd13bac81b48896e43dca6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1e68d306cd4fb4dee6b9f3ac9aa454",
+    "url": "/board-game-rules/index.html"
+  },
+  {
+    "revision": "08f36e6f9f10aef7171d",
+    "url": "/board-game-rules/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "84ea71b46a5b178d52a9",
+    "url": "/board-game-rules/static/css/main.f81377cf.chunk.css"
+  },
+  {
+    "revision": "08f36e6f9f10aef7171d",
+    "url": "/board-game-rules/static/js/2.6aa9213c.chunk.js"
+  },
+  {
+    "revision": "84ea71b46a5b178d52a9",
+    "url": "/board-game-rules/static/js/main.61c75fab.chunk.js"
+  },
+  {
+    "revision": "63e739d35c512778d777",
+    "url": "/board-game-rules/static/js/runtime-main.d35de435.js"
+  },
+  {
+    "revision": "1f11032f89010841b60ee2b6f4358b22",
+    "url": "/board-game-rules/static/media/board_game_shelf_shelves_analog_games_01-2.1f11032f.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/board-game-rules/static/media/logo.25bf045c.svg"
+  }
+]);
